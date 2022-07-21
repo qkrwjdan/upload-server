6736a6b5ee093e6ff198a15cb1e5918d81545dac
qkrwjdan <madogisa12@naver.com>
2022-07-22 05:29:44 +0900

    [feat] allow mp4

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,6 +1,6 @@
 const imageFilter = function (req, file, cb) {
   // accept image only
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif|mp4)$/)) {
     return cb(new Error("Only image files are allowed!"), false);
   }
   cb(null, true);
